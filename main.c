4740a8397444cdc0e757805a4421dd817cf18688
chriscushway <chriscushway@gmail.com>
2020-08-21 22:07:12 +0200

    remove comments

--- main.c
+++ main.c
@@ -4,11 +4,11 @@ int main(){
     
     printf("Enter a value for a: ");
     scanf("%d", &a);
-    // Add break point here using break 7
+
     printf("Enter a value for b: ");
     scanf("%d", &b);
-    // Add break point here using break 10
+
     sum = a + b;
-    // Add break point here using break 12
+    
     printf("The sum of a and b is %d \n.",sum);
 }
